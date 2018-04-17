367c98507873d3f0c0d11f75e399cec1429f5db5
David Paul <dfp333@gmail.com>
2018-04-16 19:51:00 -0500

    first db prototype

---
+++ financialdbproto.js
@@ -0,0 +1,31 @@
+const financialsDb = {
+    pgtitle:"Katy Perry Financial Interests",
+    stories:[
+        {
+            storytitle:"Financial Holdings",
+            storycontent:[
+                "Unknown number of foreign bank accounts, often under nested shell companies", 
+                "Suspected holdings of large values Cryptocurrencies. Rumors are circulating about a new PerryCoin offering.",
+                "Suspected to have convinced her most die hard fan, Steve Brownlee, of NSS, to help with the development.",
+                ˀ],
+            storyImage:{
+                img:"",
+                imgText:"Suspected Logo of \"PerryCoin\""
+            }
+        },
+        {
+            storytitle: "Money Laundering",
+            storycontent:[
+                "Launders money through though elaborate scheme of inflating number of album sales.",
+                "All research indicates no one would actually buy one of her albums."
+            ]
+        },
+        {
+            storytitle:"Tax Evasion",
+            storycontent:[
+                "ˀCreated falsified documents that showed that she had donated Massive Sums of money to the NSS program in order to evade taxes.",
+                "IRS Audit pending, will cost NSS dozens oˀf dollars of lost time to complete audit."
+            ]
+        }
+    ]
+}
