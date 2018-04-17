1a2725f74f6440b2011088f74cdbec1b149b59ce
Riley Mathews <rileymathews80@gmail.com>
2018-04-17 11:52:20 -0500

    added function to print summary

--- SummaryPrint.js
+++ SummaryPrint.js
@@ -1,7 +1,12 @@
 // function to create executive summary
 const printSummary = () => {
-    createMugshot()
-    createAssociates()
-    createAliases()
-    createCountry()
+    const article = document.querySelector("#main")
+    const fragment = document.createDocumentFragment()
+    fragment.appendChild(createMugshot())
+    fragment.appendChild(createAssociates())
+    fragment.appendChild(createAliases())
+    fragment.appendChild(createCountry())
+    article.appendChild(fragment)
 }
+
+printSummary()
