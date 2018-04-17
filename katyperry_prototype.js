6683574d3a1defd7c6d7934fbc18180ccee72eaf
pzc <paul.zimmerman.clayton@gmail.com>
2018-04-16 21:39:00 -0500

    js prototype added

---
+++ katyperry_prototype.js
@@ -0,0 +1,27 @@
+// Create main database
+const mainDatabase = {}
+
+// Create array
+mainDatabase.financialHoldings = []
+
+// Create sections
+mainDatabase.financialHoldings.push(
+  {
+    "title": "Financial Holdings",
+    "values": [
+      "Unknown number of foreign bank accounts, often under nested shell companies",
+      "Suspected holdings of large values Cryptocurrencies. Rumors are circulating about a new PerryCoin offering.",
+      "Suspected to have convinced her most die hard fan, Steve Brownlee, of NSS, to help with the development."
+    ],
+    "image": "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FmzRRT96y3Ls%2Fmaxresdefault.jpg&f=1",
+    "image_caption": `Suspected Logo of "PerryCoin"`
+  }
+{
+    "title": "Money Laundering",
+    "": "",
+    "": "",
+    "": "",
+    "": ""
+  }
+
+)
