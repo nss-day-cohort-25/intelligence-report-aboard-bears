54bee6874bba15a0c27ee6f49f3ba57603eb7932
David Paul <dfp333@gmail.com>
2018-04-16 22:33:46 -0500

    added some javascript, still need to fix images

--- financialdbproto.js
+++ financialdbproto.js
@@ -1,13 +1,18 @@
-const financialsDb = {
+const financialDb = {
+    // this would be the H1 component
     pgtitle:"Katy Perry Financial Interests",
+    // stories would probalby be each linked to a <section>
     stories:[
         {
+            // storytitle would be an h2    
             storytitle:"Financial Holdings",
+            // drop story content into a ul and foreach into li components
             storycontent:[
                 "Unknown number of foreign bank accounts, often under nested shell companies", 
                 "Suspected holdings of large values Cryptocurrencies. Rumors are circulating about a new PerryCoin offering.",
-                "Suspected to have convinced her most die hard fan, Steve Brownlee, of NSS, to help with the development.",
-                ˀ],
+                "Suspected to have convinced her most die hard fan, Steve Brownlee, of NSS, to help with the development."
+            ],
+            //storyimage img to a img tag and imgtext to 
             storyImage:{
                 img:"",
                 imgText:"Suspected Logo of \"PerryCoin\""
@@ -23,8 +28,8 @@ const financialsDb = {
         {
             storytitle:"Tax Evasion",
             storycontent:[
-                "ˀCreated falsified documents that showed that she had donated Massive Sums of money to the NSS program in order to evade taxes.",
-                "IRS Audit pending, will cost NSS dozens oˀf dollars of lost time to complete audit."
+                "Created falsified documents that showed that she had donated Massive Sums of money to the NSS program in order to evade taxes.",
+                "IRS Audit pending, will cost NSS dozens of dollars of lost time to complete audit."
             ]
         }
     ]
---
+++ finproto.js
@@ -0,0 +1,60 @@
+//create document fragment
+const finDbFrag = document.createDocumentFragment()
+
+//function to generate page
+
+const finGen = () => {
+    // <div>
+    //         <h1>Katy Perry Financial Interests </h1>
+    // </div>
+    let title = document.createElement("div")
+
+    const heading = document.createElement("h1")
+    heading.textContent = financialDb.pgtitle
+    title.appendChild(heading)
+
+    //function to iterate through stories section of db
+
+    financialDb.stories.forEach(
+        (story) => {
+            //     <section>
+            const storysection = document.createElement("section")
+            
+            //         <h2>Financial Holdings </h2>
+            const storytitle = document.createElement("h2")
+            storytitle.textContent = story.storytitle
+            storysection.appendChild(storytitle)
+            
+            //         <ul>
+            const storyUL = document.createElement("ul")
+            storysection.appendChild(storyUL) 
+
+            //create <li> for all text items
+            for (let i = 0; i < story.storycontent.length; i++) {
+                const storyContent = document.createElement("li")
+                storyContent.textContent = story.storycontent[i]
+                storyUL.appendChild(storyContent)                
+            }   
+            //         <img src="https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FmzRRT96y3Ls%2Fmaxresdefault.jpg&f=1" class="catcoin">
+            //         <figcaption>Suspected Logo of "PerryCoin"</figcaption>
+           /*  story.storyImage.forEach((image)=> {
+                const storyImg = document.createElement("img")
+                storyImg.src = image.img
+                const imgtext = document.createElement("figcaption")
+                imgtext.textContent = image.imgText
+                storysection.appendChild(storyImg)
+                storysection.appendChild(imgtext)
+                
+            }); */
+            
+            
+            //append storys to title
+            title.appendChild(storysection)
+        }
+    )
+
+   
+    finDbFrag.appendChild(title)
+}
+finGen()
+document.querySelector("#report").appendChild(finDbFrag)
